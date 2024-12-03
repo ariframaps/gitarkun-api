1d365643b270fca493433d1e2f5f691994e1d440
ariframaps <ariframa@student.ub.ac.id>
2024-12-03 09:56:03 +0700

    allow origin

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const server = http.createServer(app);
 
 // Define CORS options
 const corsOptions = {
-  origin: process.env.ORIGIN_URL, // Allow this origin. OWASP top ten
+  // origin: process.env.ORIGIN_URL, // Allow this origin. OWASP top ten
+  origin: "*", // Allow this origin. OWASP top ten
   methods: ["GET", "POST", "PUT", "DELETE"], // Allow specific HTTP methods
   allowedHeaders: ["Content-Type", "Authorization"], // Allow specific headers
   credentials: true,
